ff358cd3b4972e11385f5a96c4dd09381b48ae11
Richard <54689460+rdburton@users.noreply.github.com>
2024-09-15 09:39:19 +0100

    Two Way Teleporters
    Teleporting working both ways

--- Scripts/teleporter.gd
+++ Scripts/teleporter.gd
@@ -1,28 +1,50 @@
 extends Area3D
-class_name Teleporter
 
-var is_within: bool = false
-var is_teleporting: bool = false
 
-@onready var point_a: Node3D = $PointA
-@onready var point_b: Node3D = $PointB
+var is_within_a: bool = false
+var is_within_b: bool = false
+var is_teleporting_from_a: bool = false
+var is_teleporting_from_b: bool = false
+
+@onready var point_a: Node3D = $TorusA/PointA
+@onready var point_b: Node3D = $"../TeleporterB/TorusB/PointB"
+
 
 	
 func _process(delta: float) -> void:
-	var distance = point_b.global_position.distance_to(Player.instance.global_position)
-	if is_within and Input.is_action_just_pressed("Interact"):
+	
+	var distance_to_a = point_a.global_position.distance_to(Player.instance.global_position)
+	var distance_to_b = point_b.global_position.distance_to(Player.instance.global_position)
+	
+	if is_within_a and Input.is_action_just_pressed("Interact"):
 		print("Teleporting")
-		is_teleporting = true
-	if is_teleporting:
-		Player.instance.global_position = lerp(Player.instance.global_position, point_b.global_position, delta * 3)
-	if distance <= 5.0:
-		is_teleporting = false
+		is_teleporting_from_a = true
+	if is_teleporting_from_a:
+		Player.instance.global_position = lerp(Player.instance.global_position, point_b.global_position, delta * 5)
+		if distance_to_b <= 2.0:
+			is_teleporting_from_a = false
+	
+	if is_within_b and Input.is_action_just_pressed("Interact"):
+		print("Teleporting")
+		is_teleporting_from_b = true
+	if is_teleporting_from_b:
+		Player.instance.global_position = lerp(Player.instance.global_position, point_a.global_position, delta * 5)
+		if distance_to_a <= 2.0:
+			is_teleporting_from_b = false
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body == Player.instance:
 		print("Player in bounds")
-		is_within = true
+		is_within_a = true
 
 func _on_body_exited(body: Node3D) -> void:
-	is_within = false
+	is_within_a = false
+
+func _on_teleporter_b_body_entered(body: Node3D) -> void:
+	if body == Player.instance:
+		print("Player in bounds")
+		is_within_b = true
+
+func _on_teleporter_b_body_exited(body: Node3D) -> void:
+	is_within_b = false
