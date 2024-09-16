d39b70dc000e5e785bc24a603044ead2ad5dfe23
rdburton <54689460+rdburton@users.noreply.github.com>
2024-09-16 14:35:51 +0100

    Two-way Teleporter functionality
    You can travel between teleporters and sound plays at both ends

--- Scripts/teleporter.gd
+++ Scripts/teleporter.gd
@@ -5,37 +5,55 @@ var is_within_a: bool = false
 var is_within_b: bool = false
 var is_teleporting_from_a: bool = false
 var is_teleporting_from_b: bool = false
+var generating_teleport: bool = false
 
 @onready var point_a: Node3D = $TorusA/PointA
 @onready var point_b: Node3D = $"../TeleporterB/TorusB/PointB"
+
 @onready var teleport_audio: AudioStreamPlayer3D = $TeleportAudio
 @onready var generation_audio: AudioStreamPlayer3D = $GenerationAudio
 @onready var transit_audio: AudioStreamPlayer = $TransitAudio
 
+@onready var teleport_audio_b: AudioStreamPlayer3D = $"../TeleporterB/TeleportAudioB"
+@onready var generation_audio_b: AudioStreamPlayer3D = $"../TeleporterB/GenerationAudioB"
+@onready var transit_audio_b: AudioStreamPlayer = $"../TeleporterB/TransitAudioB"
+
+@onready var player_camera_pivot = Player.instance.camera_pivot
 
 func _process(delta: float) -> void:
+	
 	
 	var distance_to_a = point_a.global_position.distance_to(Player.instance.global_position)
 	var distance_to_b = point_b.global_position.distance_to(Player.instance.global_position)
 	
 	if is_within_a and Input.is_action_just_pressed("Interact"):
-		print("Teleporting")
+		
+		#generating_teleport = true
+		#if generating_teleport:
+			#player_camera_pivot.look_at(point_b.global_position)
+	
 		generation_audio.play()
 		await get_tree().create_timer(2).timeout
 		teleport_audio.play(0.4)
 		generation_audio.stop()
 		transit_audio.play(5.6)
-		#await get_tree().create_timer(0.8).timeout
 		is_teleporting_from_a = true
 		
 	if is_teleporting_from_a:
 		Player.instance.global_position = lerp(Player.instance.global_position, point_b.global_position, delta * 5)
 		if distance_to_b <= 2.0:
 			is_teleporting_from_a = false
+			
 	
 	if is_within_b and Input.is_action_just_pressed("Interact"):
 		print("Teleporting")
+		generation_audio_b.play()
+		await get_tree().create_timer(2).timeout
+		teleport_audio_b.play(0.4)
+		generation_audio_b.stop()
+		transit_audio_b.play(5.6)
 		is_teleporting_from_b = true
+		
 	if is_teleporting_from_b:
 		Player.instance.global_position = lerp(Player.instance.global_position, point_a.global_position, delta * 5)
 		if distance_to_a <= 2.0:
