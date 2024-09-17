3bdab8b2538989221ea81991d5bb0baba064dd17
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2024-09-18 00:34:25 +0100

    Created a new version of teleporter which can be targeted at any other instance of itself

---
+++ Scripts/teleporter_new.gd
@@ -0,0 +1,63 @@
+class_name TeleporterNew extends Area3D
+
+@export var teleporter_target : TeleporterNew
+
+@export_category("Internal")
+@export var exit_target: Node3D
+@export var portal_mesh: MeshInstance3D
+@export var teleport_audio: AudioStreamPlayer3D
+@export var generation_audio: AudioStreamPlayer3D
+@export var transit_audio: AudioStreamPlayer
+@export var timer: Timer
+
+var is_working: bool
+var is_within: bool = false
+var is_teleporting: bool
+var generating_teleport: bool = false
+var interact_timer = false
+var portal_material: ShaderMaterial
+
+func _ready() -> void:
+	portal_material = portal_mesh.get_active_material(0) as ShaderMaterial
+	portal_material.set_shader_parameter("Mix", 0.0)
+	
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	timer.timeout.connect(_on_timer_timeout)
+	
+	is_working = false
+
+func _process(delta: float) -> void:
+	fix_teleporter()
+	
+	if is_within and Input.is_action_just_pressed("Interact"):
+		if interact_timer == false:
+			interact_timer = true
+			timer.start()
+			generation_audio.play()
+			await get_tree().create_timer(2).timeout
+			teleport_audio.play(0.4)
+			generation_audio.stop()
+			transit_audio.play(5.6)
+			is_teleporting = true
+							
+	if is_teleporting:
+		Player.instance.global_position = lerp(Player.instance.global_position, teleporter_target.exit_target.global_position, delta * 5)
+		if Player.instance.global_position.distance_to(teleporter_target.exit_target.global_position) <= 2.0:
+			is_teleporting = false
+
+func fix_teleporter() -> void:
+		#Below needs to be put into 'working teleporter' function which executes once is_working = true
+	if Input.is_action_just_pressed("Interact"):
+		var tween := create_tween()
+		tween.tween_method(func(t): portal_material.set_shader_parameter("Mix", t), 0.0, 1.0, 2.0)
+		
+func _on_timer_timeout() -> void:
+	interact_timer = false
+
+func _on_body_entered(body: Node3D) -> void:
+	if body == Player.instance:
+		is_within = true
+
+func _on_body_exited(body: Node3D) -> void:
+	is_within = false
