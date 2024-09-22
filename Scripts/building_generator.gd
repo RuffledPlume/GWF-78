dfd0985a423bee7a5dcc4fcc5a233e363c4f0b54
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2024-09-22 20:51:09 +0100

    Final Changes, Added door to T2

--- Scripts/building_generator.gd
+++ Scripts/building_generator.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var lights: Node3D
+@export var door: Node3D
 @export var red_light: Node3D
 #@export var target_door: Node3D
 @export var generator_working = false
@@ -22,6 +23,7 @@ func _process(delta: float) -> void:
 			generator_audio.play()
 			lights.visible = true
 			red_light.visible = false
+			door.queue_free()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
